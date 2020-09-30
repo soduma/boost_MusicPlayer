e8e039111ca05316399664c41d98f05c984bdb29
soduma <soduma@naver.com>
2020-09-30 17:03:41 +0900

    base

--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -7,17 +7,24 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVAudioPlayerDelegate{
+    
+    var player: AVAudioPlayer!
+    var timer: Timer!
+    
+    
     @IBOutlet weak var playPauseButton: UIButton!
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var progressSlider: UISlider!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
     @IBAction func touchUpPlayPauseButton(_ sender: Any) {
         print("touch tapped")
     }

1a17cbf5428d7ecc0f4b1dc110adb6ac5dae8048
soduma <soduma@naver.com>
2020-09-30 19:29:04 +0900

    add  self.initializePlayer()

--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -23,6 +23,7 @@ class ViewController: UIViewController, AVAudioPlayerDelegate{
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.initializePlayer()
     }
     
     func initializePlayer(){

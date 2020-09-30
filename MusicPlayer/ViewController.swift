ba0869619d5e1cfd0f25e775125f1601cc658c9c
soduma <soduma@naver.com>
2020-09-30 17:46:19 +0900

    func initializePlayer

--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -25,6 +25,27 @@ class ViewController: UIViewController, AVAudioPlayerDelegate{
         // Do any additional setup after loading the view.
     }
     
+    func initializePlayer(){
+        guard let soundAsset: NSDataAsset = NSDataAsset(name: "sound")
+        else {
+            print("음원 파일을 가져올 수 없습니다.")
+            return
+        }
+        
+        do {
+            try self.player = AVAudioPlayer(data: soundAsset.data)
+            self.player.delegate = self
+        } catch let error as NSError {
+            print("플레이어 초기화 실패")
+            print("코드 : \(error.code), 메세지 : \(error.localizedDescription)")
+        }
+        self.progressSlider.maximumValue = Float(self.player.duration)
+        self.progressSlider.minimumValue = 0
+        self.progressSlider.value = Float(self.player.currentTime)
+    }
+    
+    
+    
     @IBAction func touchUpPlayPauseButton(_ sender: Any) {
         print("touch tapped")
     }
